76276594bd1dd657b7f5e251270fc818ce678020
Joshua Seet <joshu5797@gmail.com>
2019-08-02 14:40:13 +0200

    fi

--- diaBeatThis/screens/LogInScreen.js
+++ diaBeatThis/screens/LogInScreen.js
@@ -9,21 +9,23 @@ import {
     TextInput
 } from 'react-native';
 import * as firebase from 'firebase';
+import { db } from '../services/db';
 
 class LogInScreen extends Component {
 
     componentWillMount() {
-        const firebaseConfig = {
-            apiKey: "AIzaSyA0Du6Emw6ZuN9RTJkhk2Mvc1-6f0Dh-zg",
-            authDomain: "diabeatthis-ead81.firebaseapp.com",
-            databaseURL: "https://diabeatthis-ead81.firebaseio.com",
-            projectId: "diabeatthis-ead81",
-            storageBucket: "diabeatthis-ead81.appspot.com",
-            messagingSenderId: "122494668373",
-            appId: "1:122494668373:web:9ec6702aab417769"
-        };
-        // Initialize Firebase
-        firebase.initializeApp(firebaseConfig);
+        // const firebaseConfig = {
+        //     apiKey: "AIzaSyA0Du6Emw6ZuN9RTJkhk2Mvc1-6f0Dh-zg",
+        //     authDomain: "diabeatthis-ead81.firebaseapp.com",
+        //     databaseURL: "https://diabeatthis-ead81.firebaseio.com",
+        //     projectId: "diabeatthis-ead81",
+        //     storageBucket: "diabeatthis-ead81.appspot.com",
+        //     messagingSenderId: "122494668373",
+        //     appId: "1:122494668373:web:9ec6702aab417769"
+        // };
+        // // Initialize Firebase
+        // firebase.initializeApp(firebaseConfig);
+        <db />
     }
 
     static navigationOptions = {
